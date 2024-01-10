e6bc1e65bcb87fed7724f292393d37228d49f9fc
Teh Hippo <hippo@noemail.org>
2024-01-11 09:30:23 +1100

    Tidy up a bit

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,8 @@ import { init, close, calculateSignature } from './signature';
     await init()
     const uriPath = '/c/v0/user/login'
     const token = ''
-    const language = 'en'
     const timestamp = 1704924640451
-    const signature = await calculateSignature(uriPath, token, language, timestamp)
+    const signature = await calculateSignature(uriPath, token, timestamp)
     console.info(' Expected: 4c615f3877ea8f890fd4eb185e8a6b88.5245784')
     console.info(`Signature: ${signature}`)
   } finally {
--- src/signature.ts
+++ src/signature.ts
@@ -1,3 +1,4 @@
+import * as fsa from 'node:fs/promises'
 import * as fs from 'fs'
 import * as path from 'path'
 import * as http from 'http'
@@ -13,6 +14,7 @@ let page: Page | undefined
 let browser: Browser | undefined
 
 async function init (): Promise<void> {
+  await fsa.mkdir(tempFolder, { recursive: true })
   // TODO: Cache & avoid downloading again.
   await downloadTemp(
     'https://portal.energizersolar.com/js/signature.js',
@@ -22,63 +24,39 @@ async function init (): Promise<void> {
     'https://portal.energizersolar.com/js/signature.wasm',
     'signature.wasm'
   )
-  createSignature()
-  const port = startServer()
+  const port = await startServer()
   await startPuppeteer(port)
 }
 
 async function startPuppeteer (port: number): Promise<void> {
-  const address = `http://127.0.0.1:${port}`
   browser = await puppeteer.launch({
-    headless: true,
+    headless: 'new',
     args: ['--no-sandbox']
   })
   page = await browser.newPage()
-  await page.bringToFront()
-  await page.goto(address)
+  await page.goto(`http://127.0.0.1:${port}`)
   await page.waitForNetworkIdle()
 }
 
-async function calculateSignature (uriPath: string, token: string, language: string, timestamp: number): Promise<string> {
+async function calculateSignature (uriPath: string, token: string, timestamp: number): Promise<string> {
+  const language = 'en' // This doesn't impact API calls, so hard-code instead of overcomplicating params.
   if (page === undefined) throw new Error('Uninitialised')
-  console.info(`Calculating signature: ${uriPath}, ${token}, ${language}, ${timestamp}`)
+  console.debug(`Calculating signature: ${uriPath}, ${token}, ${language}, ${timestamp}`)
   const result = await page.evaluate(`
     var sign = Module.cwrap("begin_signature", "string", ["string", "string", "string", "string"]);
     sign("${uriPath}", "${token}", "${language}", "${timestamp}");`)
   return result as string
 }
 
-function createSignature (): void {
-  const destFileName = path.join(tempFolder, 'index.htm')
-  if (fs.existsSync(destFileName)) {
-    fs.rmSync(destFileName)
-  }
-  const stream = fs.createWriteStream(destFileName)
-  stream.write(`<!DOCTYPE html><!-- Credits to Roses.SolarAPI -->
-  <html lang=en>
-      <head>
-          <meta charset=utf-8>
-          <meta http-equiv=pragram content=no-cache>
-          <meta http-equiv=cache-control content="no-cache, no-store, must-revalidate">
-          <script src="signature.js"></script>
-    </head>
-    <body>
-      <script>
-          console.log("Loaded script");
-      </script></body>
-  </html>`)
-  stream.close()
-}
-
-function startServer (): number {
+async function startServer (): Promise<number> {
+  await fsa.writeFile(path.join(tempFolder, 'index.htm'), '<html><head><script src="signature.js"></script></head></html>')
   const serve = serveStatic(tempFolder, { index: ['index.htm'] })
   server = http.createServer(function onRequest (req, res) {
     serve(req, res, finalhandler(req, res))
   })
-  const addr = server.listen(0).address()
-  const port = (addr as AddressInfo)?.port
-  if (port === undefined) throw new Error('Unable to get a port')
-  return port
+  const addr = server.listen(0).address() as AddressInfo
+  if (addr === undefined) throw new Error('Unable to get a port')
+  return addr.port
 }
 
 async function close (): Promise<void> {
@@ -92,9 +70,6 @@ async function downloadTemp (
   fileName: string,
   force?: boolean
 ): Promise<void> {
-  if (!fs.existsSync(tempFolder)) {
-    fs.mkdirSync(tempFolder)
-  }
   const destFileName = path.join(tempFolder, fileName)
   if (fs.existsSync(destFileName) && force !== true) {
     return
