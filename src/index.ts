b2f83c15c5f61df8c9d60287e572ccd7edc7f147
Teh Hippo <hippo@noemail.org>
2024-01-11 09:12:41 +1100

    Signature working as expected

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import { init, close, calculateSignature } from './signature';
 (async () => {
   try {
     await init()
-    const uriPath = '/api/login'
+    const uriPath = '/c/v0/user/login'
     const token = ''
     const language = 'en'
-    const timestamp = 12341283497
+    const timestamp = 1704924640451
     const signature = await calculateSignature(uriPath, token, language, timestamp)
+    console.info(' Expected: 4c615f3877ea8f890fd4eb185e8a6b88.5245784')
     console.info(`Signature: ${signature}`)
   } finally {
     await close()
