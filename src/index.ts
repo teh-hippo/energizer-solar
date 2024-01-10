a91adb3fbc417468986308a1aaa3b416a00c34f5
Teh Hippo <hippo@noemail.org>
2024-01-10 17:33:59 +1100

    FINAFREAKINGLY I HATE TYPESCRIPT

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
 
 import { Person } from './person';
-import { } from 'https';
+import { get } from 'https';
+import { existsSync, mkdirSync, createWriteStream } from 'fs';
+import { join } from 'path';
 
-let person = new Person('Bob', 34);
+async function downloadTemp(url: string, fileName: string): Promise<void> {
+    const tempFolder: string = "temp";
+    if (!existsSync(tempFolder)) {
+        mkdirSync(tempFolder);
+    }
+    const destFileName = join(tempFolder, fileName);
+    await new Promise<void>((resolve,reject) => {
+        const request = get(url, response => {
+            const stream = createWriteStream(destFileName);
+            response.pipe(stream);
+            stream.on("finish", () => {
+                stream.close();
+            })
+            .on("close", () => {
+                resolve();
+            })
+            .on("error", e => {
+                console.error(e);
+                reject(e.message);
+            });
+        }).on("error", e => {
+            console.error(e);
+            reject(e.message);
+        });
+                
+    });
+}
 
-console.log(person.getGreeting());
+(async() => {
+    let person = new Person('Bob', 34);
+    console.log(person.getGreeting());
+    await downloadTemp("https://www.google.com", "index.html");
+    console.log("done");
+})();
